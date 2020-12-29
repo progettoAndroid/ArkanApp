220dacde063233b153f02424902d4c749f0e63ea
cosimo <cosimopolito93@libero.it>
2020-12-29 22:37:29 +0100

    aggiunto secondo livello ai mattoncini

--- app/src/main/java/com/example/android/arkanoid/Brick.java
+++ app/src/main/java/com/example/android/arkanoid/Brick.java
@@ -5,20 +5,53 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.view.View;
 
+import java.util.HashMap;
+
+
 public class Brick extends View {
 
-    private Bitmap brick;
+
+    public enum Level {
+        ONE,
+        TWO
+    };
+
+    public Level getLevel() {
+        return level;
+    }
+
+    public void setLevel(Level level) {
+        this.level = level;
+    }
+
+    private  Level level;
+    private Bitmap   brick;
     private float x;
     private float y;
 
-    public Brick(Context context, float x, float y) {
+
+    public Brick(Context context, float x, float y ,Level level) {
         super(context);
         this.x = x;
         this.y = y;
-        skin();
+        this.level = level;
+        if (level == Level.ONE)
+            skin();
+        else if (level == Level.TWO)
+            skin();
+    }
+    public Brick(Context context, float x, float y ,Level level,Bitmap bitmapCurrent) {
+        super(context);
+        this.x = x;
+        this.y = y;
+        if (level == Level.ONE)
+            skin();
+        else if (level == Level.TWO)
+            skin_level2(bitmapCurrent);
     }
 
-    // priradi random obrazok tehlicke
+
+    // assegna un'immagine casuale al mattone
     private void skin() {
         int a = (int) (Math.random() * 8);
         switch (a) {
@@ -27,9 +60,9 @@ public class Brick extends View {
                 break;
             case 1:
                 brick = BitmapFactory.decodeResource(getResources(), R.drawable.brick_blue);
-                break;
+                 break;
             case 2:
-                brick = BitmapFactory.decodeResource(getResources(), R.drawable.brick_green);
+                 brick = BitmapFactory.decodeResource(getResources(), R.drawable.brick_green);
                 break;
             case 3:
                 brick = BitmapFactory.decodeResource(getResources(), R.drawable.brick_orange);
@@ -48,6 +81,38 @@ public class Brick extends View {
                 break;
         }
     }
+
+    // assegna un'immagine casuale al mattone
+    private void skin_level2( Bitmap bitmapCurrent) {
+        int a = (int) (Math.random() * 8);
+        switch (a) {
+            case 0:
+                brick = BitmapFactory.decodeResource(getResources(), R.drawable.brick_aqua_screpolato);
+                break;
+            case 1:
+                brick = BitmapFactory.decodeResource(getResources(), R.drawable.brick_blue_screpolato);
+                break;
+            case 2:
+                brick = BitmapFactory.decodeResource(getResources(), R.drawable.brick_green_screpolato);
+                break;
+            case 3:
+                brick = BitmapFactory.decodeResource(getResources(), R.drawable.brick_orange_screpolato);
+                break;
+            case 4:
+                brick = BitmapFactory.decodeResource(getResources(), R.drawable.brick_pink_screpolato);
+                break;
+            case 5:
+                brick = BitmapFactory.decodeResource(getResources(), R.drawable.brick_purple_screpolato);
+                break;
+            case 6:
+                brick = BitmapFactory.decodeResource(getResources(), R.drawable.brick_red_screpolato);
+                break;
+            case 7:
+                brick = BitmapFactory.decodeResource(getResources(), R.drawable.brick_yellow_screpolato);
+                break;
+        }
+    }
+
 
     @Override
     public float getX() {
