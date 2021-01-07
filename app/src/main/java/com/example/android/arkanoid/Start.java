bf03b74017b0a1358faea02906c3e804364e5453
Giannella <danielegiannella@outlook.it>
2021-01-07 18:21:44 +0100

    Aggiunta Splash Screen

---
+++ app/src/main/java/com/example/android/arkanoid/Start.java
@@ -0,0 +1,25 @@
+package com.example.android.arkanoid;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Start extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start2);
+        int timeout = 1000;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Start.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },timeout);
+    }
+}
